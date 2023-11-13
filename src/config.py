1910711a6ac28d8a0f23817a656d03b9d0d63be2
volodkindv <volodkindv@skbkontur.ru>
2023-11-13 18:00:08 +0500

    4 workers

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@ Love It? Star It! ⭐ https://github.com/omkarcloud/google-maps-scraper/
 '''
 queries = [
     {
-        "keyword": "vet clinics Sydney",
+        "keyword": "vet clinics Yekaterinburg",
     },
 ]
 
-number_of_scrapers = 1
+number_of_scrapers = 4
